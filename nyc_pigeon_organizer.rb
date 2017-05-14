c9ff690811836b5c4815f5936cfe23783db4a8cc
Kenny Hearn <wkhearn@gmail.com>
2017-05-14 14:41:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+
+  data.each do |trait, types|
+    types.each do |type, pigeons|
+      pigeons.each do |pigeon|
+
+        pigeon_hash[pigeon] ||= {}
+        pigeon_hash[pigeon][trait] ||= []
+        pigeon_hash[pigeon][trait] << type.to_s
+
+      end
+    end
+  end
+
+  pigeon_hash
+
 end
